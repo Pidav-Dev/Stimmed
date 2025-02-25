59df06cc0393cda9196d4e2b90f4d89c3799a71e
Luca Maria Incarnato <lucaincarnato.03@gmail.com>
2025-02-25 16:23:08 +0100

    UI prototype
    Created the prototype for the endurance display, as text, and the pause button, without functionality

--- Assets/Scripts/EnduranceManager.cs
+++ Assets/Scripts/EnduranceManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UIElements; 
 using System.Collections;
 
 public class EnduranceManager : MonoBehaviour
@@ -8,9 +9,14 @@ public class EnduranceManager : MonoBehaviour
     private float minEndurance = 0f;
     private float enduranceDecreaseRate = 2f; // Speed of endurance decrease per second
     private float decreaseInterval = 1f; // Time interval for decrease (1 second)
+    
+    [SerializeField] private UIDocument uiDocument;
+    private Label enduranceLabel;
 
     private void Awake()
     {
+        enduranceLabel = uiDocument.rootVisualElement.Q<Label>("Endurance");
+        
         if (Instance == null)
             Instance = this;
         else
@@ -25,6 +31,11 @@ public class EnduranceManager : MonoBehaviour
     private void Start()
     {
         StartCoroutine(DecreaseEndurance());
+    }
+
+    private void Update()
+    {
+        enduranceLabel.text = endurance.ToString();
     }
 
     private IEnumerator DecreaseEndurance()
