cc3f5ac407520f847240a5ee5c7ae68fe7719a75
Luca Maria Incarnato <lucaincarnato.03@gmail.com>
2025-02-27 11:17:29 +0100

    Difficulty balancing
    Removed endurance responsability from character and added to stimuli.
    Made stimulus amount and time modular

--- Assets/Scripts/StateChange.cs
+++ Assets/Scripts/StateChange.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class StateChange : MonoBehaviour
 {
+    [SerializeField] private int stimulusAmount = 2; 
+    [SerializeField] private float stimulusInterval = 1f;
+    [SerializeField] private EnduranceManager character;
     private bool _isActive; // Determine if the stimuli is sensory overloading 
     private Renderer _objectRenderer; // Get the renderer of the actual object
     private Color _originalColor; // Get renderer's color
@@ -16,10 +19,11 @@ public class StateChange : MonoBehaviour
             _originalColor = _objectRenderer.material.color;
         }
         StartCoroutine(ActivateRandomly()); // Start the concurrent routine of the random activation
+        StartCoroutine(IncreaseEndurance()); // Start the concurrent routine of the random activation
     }
 
     // Concurrently activates the stimuli with a random wait time
-    IEnumerator ActivateRandomly()
+    private IEnumerator ActivateRandomly()
     {
         // Produces warning but it is needed to allow infinite looping 
         while (true)
@@ -35,30 +39,31 @@ public class StateChange : MonoBehaviour
             }
         }
     }
+    
+    private IEnumerator IncreaseEndurance()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(stimulusInterval); // Before incrementing, it waits for some time
+            if (_isActive)
+            {
+                character.SetEndurance(stimulusAmount);
+            }
+        }
+    }
 
     // Increase endurance once the user clicks down
     void OnMouseDown()
     {
-        if (!_isActive) return; 
-        // Execute only if isActive 
+        if (!_isActive) return; // Execute only if isActive 
         _isActive = false;
         ChangeColor(_originalColor); // Change element's color
         // Increase universal endurance when clicking any active object
-        if (EnduranceManager.Instance != null)
-        {
-            Debug.Log("Before Reduction: " + EnduranceManager.Instance.GetEndurance());
-            EnduranceManager.Instance.IncreaseEnduranceByAmount(10); // Integer reduction
-            Debug.Log("After Reduction: " + EnduranceManager.Instance.GetEndurance());
-        }
-        else
-        {
-            Debug.LogError("EnduranceManager instance is missing from the scene!");
-        }
-        Debug.Log(gameObject.name + " Deactivated (Color: Original)");
+        character.SetEndurance(-2*stimulusAmount);
     }
 
     // Changes renderer's color
-    void ChangeColor(Color newColor)
+    private void ChangeColor(Color newColor)
     {
         // Instead of _objectRenderer != null, too expensive 
         if (_objectRenderer)
