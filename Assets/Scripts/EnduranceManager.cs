3fd8f1342802029b6dc204897e154345a55222e5
Luca Maria Incarnato <lucaincarnato.03@gmail.com>
2025-02-28 10:45:58 +0100

    Endurance UI draft
    Endurance starts from the top left and bottom right corner and fill the whole frame when it is 100

--- Assets/Scripts/EnduranceManager.cs
+++ Assets/Scripts/EnduranceManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 using System.Collections;
@@ -9,17 +10,26 @@ public class EnduranceManager : MonoBehaviour
     [SerializeField] private UIDocument uiDocument;
     private const int MaxEndurance = 100; // Means character is sensory overloaded and you lost
     private Label _enduranceLabel; // Label where it will be attached 
+    private VisualElement _topFill;
+    private VisualElement _rightFill;
+    private VisualElement _bottomFill;
+    private VisualElement _leftFill;
     
     private void Start()
     {
         endurance = 0; 
         _enduranceLabel = uiDocument.rootVisualElement.Q<Label>("Endurance"); // Find the UI element in UXML file and attach to it
+        _topFill = uiDocument.rootVisualElement.Q<VisualElement>("top-fill"); // Name your fill element in UI Builder
+        _rightFill = uiDocument.rootVisualElement.Q<VisualElement>("right-fill"); // Name your fill element in UI Builder
+        _bottomFill = uiDocument.rootVisualElement.Q<VisualElement>("bottom-fill"); // Name your fill element in UI Builder
+        _leftFill = uiDocument.rootVisualElement.Q<VisualElement>("left-fill"); // Name your fill element in UI Builder
     }
 
     private void Update()
     {
         // Update label
         _enduranceLabel.text = endurance.ToString();
+        UpdateProgressBar();
     }
 
     // Returns endurance following information hiding principles
@@ -31,5 +41,15 @@ public class EnduranceManager : MonoBehaviour
     public void SetEndurance(int amount)
     {
         endurance = Mathf.Clamp(endurance + amount, 0, MaxEndurance);
+    }
+
+    private void UpdateProgressBar()
+    {
+        float topAmount = Mathf.Clamp01(Math.Clamp(endurance, 0, MaxEndurance)/100f);
+        _topFill.transform.scale = new Vector3(topAmount, 1, 1);
+        _leftFill.transform.scale = new Vector3(1, topAmount, 1);
+        float bottomAmount = Mathf.Clamp01(Math.Clamp(endurance, 0, MaxEndurance)/100f);
+        _bottomFill.transform.scale = new Vector3(bottomAmount, 1, 1);
+        _rightFill.transform.scale = new Vector3(1, bottomAmount, 1);
     }
 }
