257ecb79b40cd82a794032d245bfa2cb3b9e6619
Luca Maria Incarnato <lucaincarnato.03@gmail.com>
2025-02-28 17:27:45 +0100

    GameOver panel added
    When player looses the game over panel is shown and replay can be done

--- Assets/Scripts/StateMenu.cs
+++ Assets/Scripts/StateMenu.cs
@@ -1,27 +1,36 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
-public class PauseMenu : MonoBehaviour
+public class StateMenu : MonoBehaviour
 {
     [SerializeField] private UIDocument uiDocument; // Assign this in the Inspector!
     // UI elements
     private Button _pauseButton;
     private VisualElement _pausePanel;
+    private VisualElement _gameOverPanel;
     private Button _resumeButton;
     private Button _quitButton;
+    private Button _replayButton;
+    
+    [SerializeField] private EnduranceManager _enduranceManager;
+    
 
     private void Awake()
     {
         // Find UI elements (should add if (element == null) Debug.LogError("Element not found!"); but bored)
         _pauseButton = uiDocument.rootVisualElement.Q<Button>("Pause");
         _pausePanel = uiDocument.rootVisualElement.Q<VisualElement>("PausePanel");
+        _gameOverPanel = uiDocument.rootVisualElement.Q<VisualElement>("GameOverPanel");
         _resumeButton = uiDocument.rootVisualElement.Q<Button>("ResumeButton");
         _quitButton = uiDocument.rootVisualElement.Q<Button>("QuitButton");
+        _replayButton = uiDocument.rootVisualElement.Q<Button>("ReplayButton");
 
         // Add event listeners
         _pauseButton.clicked += TogglePause;
         _resumeButton.clicked += ResumeGame;
         _quitButton.clicked += QuitGame;
+        _replayButton.clicked += ReplayGame;
     }
 
     private void Update()
@@ -29,6 +38,10 @@ public class PauseMenu : MonoBehaviour
         // Checks if escape button is pressed and then toggles pause (for debugging only) 
         if (Input.GetKeyDown(KeyCode.Escape))
             TogglePause();
+        if (_enduranceManager && _enduranceManager.GetEndurance() == 100)
+        {
+            _gameOverPanel.style.display = DisplayStyle.Flex;
+        }
     }
 
     // Called when the pause button is pressed 
@@ -54,5 +67,11 @@ public class PauseMenu : MonoBehaviour
         #else
         Application.Quit(); // Uses the particular build's way of closing the app if it is not running in the Unity Editor
         #endif
+    }
+
+    private void ReplayGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
